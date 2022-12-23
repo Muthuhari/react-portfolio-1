21e60e0acdcf8db69a83bb7e5925bf0a701dab1c
MUthuhari <muthuharinisansala2@gmail.com>
2022-12-23 09:07:13 +0530

    added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
               <article className='about__card'>
               <FiUsers className='=about__icon'/>
               <h5>Clients</h5>
-              <small>200+ worlwide(Not yet)</small>
+              <small>100+ worlwide(Not yet)</small>
               </article>
 
               <article className='about__card'>
